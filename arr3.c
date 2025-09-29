32e932dcf6c9a3b59877e2f17bc6eada7a3f579b
parthst12-ux <parthst12@gmail.com>
2025-09-29 20:57:11 +0530

    Add files via upload

---
+++ arr3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main ()
+{
+int n,i;
+int arr[100];
+int max,min;
+scanf("%d", &n);
+
+for(i = 0;i<n;i++){
+    scanf("%d", arr[i]);
+    max = arr[0];
+    min = arr[0];
+    for(i = 0;i<n;i++){
+    if(arr[i] > max){
+        max = arr[i];
+
+    }
+    if(arr[i] < min){
+        min = arr[i];
+
+    }
+
+    }
+    printf("Max=%d,Min=%d", max,min);
+}
+return 0;
+}
